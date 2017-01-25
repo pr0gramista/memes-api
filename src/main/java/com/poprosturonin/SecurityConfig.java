26debd1b7279e52221c7f1474a15be634ad28410
Bartosz Wiśniewki <poprosturonin@gmail.com>
2017-01-26 00:31:22 +0100

    Authentication now works by basic HTTP

--- src/main/java/com/poprosturonin/SecurityConfig.java
+++ src/main/java/com/poprosturonin/SecurityConfig.java
@@ -14,6 +14,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/manage/*")
                 .authenticated()
                 .anyRequest()
-                .permitAll();
+                .permitAll()
+                .and()
+                .httpBasic();
     }
 }
