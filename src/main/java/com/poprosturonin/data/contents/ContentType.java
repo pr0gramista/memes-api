dc05f35043fdd9ee53225de30a554e3c040259df
Bartosz Wiśniewski <poprosturonin@gmail.com>
2017-02-23 22:07:45 +0100

    Preview content

--- src/main/java/com/poprosturonin/data/contents/ContentType.java
+++ src/main/java/com/poprosturonin/data/contents/ContentType.java
@@ -4,5 +4,5 @@ package com.poprosturonin.data.contents;
  * Content type
  */
 public enum ContentType {
-    IMAGE, VIDEO, GALLERY, CAPTIONED_GALLERY, GIF
+    IMAGE, VIDEO, GALLERY, CAPTIONED_GALLERY, GIF, PREVIEW
 }
---
+++ src/main/java/com/poprosturonin/data/contents/PreviewContent.java
@@ -0,0 +1,16 @@
+package com.poprosturonin.data.contents;
+
+/**
+ * Preview of long meme that need to accessed
+ * by clicking on it
+ */
+public class PreviewContent extends ImageContent {
+    public PreviewContent(String url) {
+        super(url);
+    }
+
+    @Override
+    public ContentType getContentType() {
+        return ContentType.IMAGE;
+    }
+}
