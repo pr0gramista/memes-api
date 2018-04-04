b53c75cb82a89b66cf9db74d8cf1fe892991909c
Bartosz Wiśniewski <poprosturonin@gmail.com>
2018-04-05 00:37:20 +0200

    Move to newer Spring and Gradle version

--- src/main/java/com/poprosturonin/SecurityConfig.java
+++ src/main/java/com/poprosturonin/SecurityConfig.java
@@ -1,9 +1,11 @@
 package com.poprosturonin;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.firewall.StrictHttpFirewall;
 
 @Configuration
 @EnableWebSecurity
@@ -17,5 +19,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .permitAll()
                 .and()
                 .httpBasic();
+    }
+
+    @Bean
+    public StrictHttpFirewall httpFirewall() {
+        StrictHttpFirewall firewall = new StrictHttpFirewall();
+        firewall.setAllowBackSlash(true);
+        firewall.setAllowUrlEncodedPercent(true);
+        return firewall;
     }
 }
